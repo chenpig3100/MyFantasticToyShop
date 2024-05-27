c157334d0a6581abf543b2dc08a13b3d7f330f5b
chenpig3100 <122781978+chenpig3100@users.noreply.github.com>
2024-05-27 23:24:07 +1000

    Jinja Restructure
    Restructure complete except ProductDetail function.

--- N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/__init__.py
+++ N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app=Flask(__name__)
 def create_app():
-    app=Flask(__name__)
+    
     app.debug=True
 
     from . import views
     app.register_blueprint(views.bp)
 
     return app
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template("500.html")
