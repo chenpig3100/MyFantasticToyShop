303857c8b1c9b23ec8eb282f5801115c5893276c
w17ian <ianwangian9@gmail.com>
2024-05-31 18:16:03 +1000

    add fuction but not work
    need to restructure

---
+++ IFN557_assignment-Ian/N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/models.py
@@ -0,0 +1,84 @@
+# class Toy:
+#     def __init__(self, id, image, name, price, category, description):
+#         self.id = id
+#         self.image = image
+#         self.name = name
+#         self.price = price
+#         self.category = category
+#         self.description = description
+
+#     def get_toy_detail(self):
+#         return str(self)
+    
+#     def __repr__(self):
+#         str = "ID: {}, Image: {}, Name: {}, Price: {}, Category: {}, description: {} \n"
+#         str = str.format(self.id, self.image, self.name, self.price, self.category, self.description)
+#         return str
+
+from . import db
+
+class Toy(db.Model):
+    __tablename__ = 'toys'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    image = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    description = db.Column(db.String(500), nullable=False, default='There is no description.')
+
+    def __repr__(self):
+         str = "ID: {}, Name: {}, Image: {}, Price: {}, Category_id: {}, Description: {}\n"
+         str = str.format(self.id, self.name, self.image, self.price, self.category_id, self.description)
+         return str
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True) 
+    toy_id = db.Column(db.Integer, db.ForeignKey('toys.id'), primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        str = "ID: {}, Toy ID: {}, Order Detail ID: {}, Amount: {}, Total Price: {}\n" 
+        str = str.format(self.id, self.toy_id, self.order_detail_id, self.amount, self.total_price)
+        return str
+    
+    
+class Order_detail(db.Model):
+    __tablename__ = 'order_details'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    firstname = db.Column(db.String(64), nullable=False)
+    lastname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        str = "ID: {}, First Name: {}, Last Name: {}, Email: {}, Address: {}\n" 
+        str = str.format(self.id, self.firstname, self.lastname, self.email, self.address)
+        return str
+    
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+
+    def __repr__(self):
+        str = "ID: {}, Name: {}\n"
+        str = str.format(self.id, self.name)
+        return super().__repr__()
+
+class ContactUs(db.Model):
+    __tablename__ = 'contactus'
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(64))
+    lastname = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    comment = db.Column(db.String(500))
+
+    def __repr__(self):
+        str = "ID: {}, First Name: {}, Last Name: {}, Email: {}, Comment: {}\n"
+        str = str.format(self.id, self.firstname, self.lastname, self.email, self.comment)
+        return str
+
+    
---
+++ IFN557_assignment-Ian/N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/views.py
@@ -0,0 +1,127 @@
+from flask import Blueprint, render_template, url_for, request, session, flash, redirect
+from .models import Toy, Order_detail, Order
+from . import db
+# This data will eventually be stored in a database
+# HelloKitty = Toy(1, 'HelloKitty.jpg', 'HelloKitty', '$10', 'plushi','This adorable Hello Kitty plushie features soft, cuddly fabric and a cute pink bow. With her friendly smile, she is perfect for hugs, decoration, and gifting to Hello Kitty fans of all ages.')
+# IronMan = Toy(2, 'ironMan.jpg', 'IronMan', '$100', 'collection', 'Marvel at the ultimate Iron Man collection: suits, gadgets, and comics galore, embodying heroism, innovation, and technological prowess.')
+# toys = [HelloKitty, IronMan]
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    toys = Toy.query.order_by(Toy.name).all()
+    # return render_template('Homepage.html', toybox = toys)
+    return render_template('Homepage.html', toys = toys)
+
+@bp.route('/category/<int:category>')
+def category(category):
+    match category:
+        case 1:
+            toys = Toy.query.filter(Toy.category_id == category).all()
+        case 2:
+            toys = Toy.query.filter(Toy.category_id == category).all()
+        case 3:
+            toys = Toy.query.filter(Toy.category_id == category).all()
+        case 4:
+            toys = Toy.query.order_by(Toy.name).all()
+
+    return render_template('Homepage.html', toys = toys)
+    
+@bp.route('/toys')
+def search():
+    search = request.args.get('search')
+    search = '%{}%'.format(search)
+    toys = Toy.query.filter(Toy.name.like(search)).all()
+    return render_template('Homepage.html', toys = toys)
+
+
+# @bp.route('/ProductDetailPage/<int:toyid>/')
+# def toys(toyid):
+#     newToyBoxs = []
+#     for Toy in toys:
+#         if int(Toy.id) == int(toyid):
+#             newToyBoxs.append(Toy)
+#     return render_template('ProductDetailPage.html', toybox = newToyBoxs)
+
+@bp.route('/toy/<int:toyid>/')
+def toydetail(toyid):
+    toys = Toy.query.all()
+    toy = Toy.query.filter(Toy.id == toyid).first()
+    print(toy.image)
+    return render_template('ProductDetailPage.html', toy = toy, toys = toys)
+
+
+
+#orderDetail function (Ian)
+@bp.route('/checkout', methods=['POST','GET'])
+def checkout():
+    order_detail = Order_detail(order_id = request.values.get('order_id'), 
+                                firstname = request.values.get('firstname'), 
+                                lastname = request.values.get('lastname'),
+                                email = request.values.get('email'),
+                                address = request.values.get('address'))
+    
+    try:
+        db.session.add(order_detail)
+        db.session.commit()
+        flash('Thanks for order')
+        return redirect(url_for('main.index'))
+    
+    except:
+        flash('There was an issue completing your order')
+        return redirect(url_for('main.index'))    
+
+@bp.route('/check')
+def check():
+    return render_template('Orderdetail.html')
+
+@bp.route('/add/<int:toyid>/')
+def add(toyid):
+    order = checkSession()
+    toy = Toy.query.filter(Toy.id == toyid).first()
+
+    order_check = Order.query.where((Order.toy_id == toyid) & (Order.id == order.id))
+    if order_check == None:
+        print('check(1')
+        o1 = Order()
+        o1.id = order.id
+        o1.toy_id = toyid
+        o1.amount = 1
+        o1.total_price = toy.price * o1.amount
+        db.session.add(o1)
+    else:
+        print('check(2')
+        order_check.amount += 1
+        order_check.total_price = toy.price * order_check.amount
+
+    try:
+        db.session.commit()
+        flash('add toy complete')
+    except Exception as e:
+        print('check(3)')
+        print(e)
+        flash('add failed')
+
+
+
+def checkSession():
+    if 'order_id' in session.keys():
+        order = db.session.scalar(db.select(Order).where(Order.id==session['order_id']).first())
+
+    else:
+        order = None
+
+    if order is None:
+        order = Order()
+        try:
+            db.session.add(order)
+            db.session.commit()
+            session['order_id']=order.id
+        except Exception as e:
+            print(e)
+            print('Fail')
+
+    return order
+
+
