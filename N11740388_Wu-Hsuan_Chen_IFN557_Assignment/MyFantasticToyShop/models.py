5274dd4fb8c3a0ad08dbc7021a953a72738f187a
chenpig3100 <122781978+chenpig3100@users.noreply.github.com>
2024-06-01 16:27:27 +1000

    Database Rebuild
    Rebuild success!!

--- N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/models.py
+++ N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/models.py
@@ -13,33 +13,37 @@ class Toy(db.Model):
          str = "ID: {}, Name: {}, Image: {}, Price: {}, Category_id: {}, Description: {}\n"
          str = str.format(self.id, self.name, self.image, self.price, self.category_id, self.description)
          return str
-
-class Order(db.Model):
-    __tablename__ = 'orders'
-    id = db.Column(db.Integer, primary_key=True) 
-    toy_id = db.Column(db.Integer, db.ForeignKey('toys.id'), primary_key=True)
-    amount = db.Column(db.Integer, nullable=False)
-    total_price = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        str = "ID: {}, Toy ID: {}, Order Detail ID: {}, Amount: {}, Total Price: {}\n" 
-        str = str.format(self.id, self.toy_id, self.order_detail_id, self.amount, self.total_price)
-        return str
-    
     
 class Order_detail(db.Model):
     __tablename__ = 'order_details'
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
-    firstname = db.Column(db.String(64), nullable=False)
-    lastname = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), nullable=False)
-    address = db.Column(db.String(64), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    toy_id = db.Column(db.Integer, db.ForeignKey('toys.id'), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    toy = db.relationship('Toy', backref='order_details')
+    def __repr__(self):
+        str = "ID: {}, Order ID: {}, Toy ID: {}, Amount: {}, Total Price: {}\n" 
+        str = str.format(self.id, self.order_id, self.toy_id, self.amount, self.total_price)
+        return str
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True) 
+    firstname = db.Column(db.String(64), nullable=True)
+    lastname = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(64), nullable=True)
+    address = db.Column(db.String(64), nullable=True)
+    order_details = db.relationship('Order_detail', backref='orders', lazy=True)
 
     def __repr__(self):
-        str = "ID: {}, First Name: {}, Last Name: {}, Email: {}, Address: {}\n" 
+        str = "ID: {}, Firstname: {}, Lastname: {}, Email: {}, Address: {}\n" 
         str = str.format(self.id, self.firstname, self.lastname, self.email, self.address)
         return str
+    
+    
+
     
 class Category(db.Model):
     __tablename__ = 'categories'
