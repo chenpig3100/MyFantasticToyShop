50c74ec5e5549a09d924c7eca59db9e9d59f1b75
chenpig3100 <122781978+chenpig3100@users.noreply.github.com>
2024-06-01 15:30:47 +1000

    Code mapping
    Finish code mapping.

--- N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/models.py
+++ N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/models.py
@@ -1,20 +1,3 @@
-# class Toy:
-#     def __init__(self, id, image, name, price, category, description):
-#         self.id = id
-#         self.image = image
-#         self.name = name
-#         self.price = price
-#         self.category = category
-#         self.description = description
-
-#     def get_toy_detail(self):
-#         return str(self)
-    
-#     def __repr__(self):
-#         str = "ID: {}, Image: {}, Name: {}, Price: {}, Category: {}, description: {} \n"
-#         str = str.format(self.id, self.image, self.name, self.price, self.category, self.description)
-#         return str
-
 from . import db
 
 class Toy(db.Model):
--- N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/views.py
+++ N11740388_Wu-Hsuan_Chen_IFN557_Assignment/MyFantasticToyShop/views.py
@@ -57,4 +57,108 @@ def submit():
 
     return redirect(url_for('main.index'))
 
+# checkout function #Bryan
+@bp.route('/checkout', methods=['POST','GET'])
+def checkout():
+    order_detail = Order_detail(order_id = request.values.get('order_id'), 
+                                firstname = request.values.get('firstname'), 
+                                lastname = request.values.get('lastname'),
+                                email = request.values.get('email'),
+                                address = request.values.get('address'))
+    
+    try:
+        db.session.add(order_detail)
+        db.session.commit()
+        flash('Thanks for order')
+        return redirect(url_for('main.index'))
+    
+    except:
+        flash('There was an issue completing your order')
+        return redirect(url_for('main.index')) 
 
+# navigate to orderdetail page #Bryan 
+@bp.route('/check')
+def check():
+    return render_template('Orderdetail.html')
+
+# Add toy function #Bryan
+# @bp.route('/add/<int:toyid>/')
+# def add(toyid):
+
+    
+#     toy = Toy.query.get_or_404(toyid)
+#     #check order exist
+#     if 'order_id' in session:
+#         order_id = session['order_id']
+#         order = db.session.scalar(db.select(Order).where(Order.id==session['order_id']).first())
+#         #order_check = Order.query.where((Order.toy_id == toyid) & (Order.id == order.id))
+
+#         if order_check is None:
+#             o1 = Order()
+#             o1.id = order.id
+#             o1.toy_id = toyid
+#             o1.amount = 1
+#             o1.total_price = toy.price * o1.amount
+#             db.session.add(o1)
+#             session['order_id']=order.id
+#         else:
+#             order_check.amount += 1
+#             order_check.total_price = toy.price * order_check.amount
+#     else:
+
+#         o1 = Order()
+#         o1.toy_id = toyid
+#         o1.amount = 1
+#         o1.total_price = toy.price * o1.amount
+#         db.session.add(o1)
+#         session['order_id']=o1.id
+
+#     try:
+#         db.session.commit()
+#         flash('add toy complete')
+#     except Exception as e:
+#         print(e)
+
+#     return redirect(url_for('main.index'))  
+
+
+#check toyid exist in order 
+    # toy = Toy.query.filter(Toy.id == toyid).first()
+
+    # order_check = Order.query.where((Order.toy_id == toyid) & (Order.id == order.id))
+    # if order_check is None:
+    #     o1 = Order()
+    #     o1.toy_id = toyid
+    #     o1.amount = 1
+    #     o1.total_price = toy.price * o1.amount
+    #     db.session.add(o1)
+    #     session['order_id']=order.id
+    # else:
+    #     order_check.amount += 1
+    #     order_check.total_price = toy.price * order_check.amount
+
+    # try:
+    #     db.session.commit()
+    #     flash('add toy complete')
+    # except Exception as e:
+    #     print(e)
+        
+# check have order.id exist or not #Bryan
+# def checkSession():
+#     if 'order_id' in session.keys():
+#         order = db.session.scalar(db.select(Order).where(Order.id==session['order_id']).first())
+
+#     else:
+#         order = None
+
+#     if order is None:
+#         # Order 在創建的時候要Order.id 與 Toy.id 一起給，兩個都是PK
+#         order = Order()
+#         try:
+#             db.session.add(order)
+#             db.session.commit()
+#             session['order_id']=order.id
+#         except Exception as e:
+#             print(e)
+
+#     return order
